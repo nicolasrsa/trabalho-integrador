2db7fa65618956c80e9cf24e40bcb35f6d0ce836
Lucas Moraes <lucashsmoraes@gmail.com>
2018-09-23 17:43:21 -0300

    Classe Motorista e Roteiro

--- Transportadora/src/br/com/transportadora/model/Motorista.java
+++ Transportadora/src/br/com/transportadora/model/Motorista.java
@@ -1,6 +1,32 @@
 package br.com.transportadora.model;
 
 
-class Motorista {
-    
+public class Motorista extends Pessoa{
+  
+  private Roteiro roteiro;
+  private Veiculo veiculo;
+  
+  public Motorista(){}
+
+  public Motorista(Roteiro roteiro, Veiculo veiculo, String nome, Endereco edereco) {
+    super(nome, edereco);
+    this.roteiro = roteiro;
+    this.veiculo = veiculo;
+  }  
+
+  public Roteiro getRoteiro() {
+    return roteiro;
+  }
+
+  public void setRoteiro(Roteiro roteiro) {
+    this.roteiro = roteiro;
+  }
+
+  public Veiculo getVeiculo() {
+    return veiculo;
+  }
+
+  public void setVeiculo(Veiculo veiculo) {
+    this.veiculo = veiculo;
+  }  
 }
--- Transportadora/src/br/com/transportadora/model/Roteiro.java
+++ Transportadora/src/br/com/transportadora/model/Roteiro.java
@@ -1,6 +1,92 @@
 package br.com.transportadora.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 
 public class Roteiro {
-    
+  
+  private Long codigo;
+  private List<Long> entregasRealizadas = new ArrayList<>();
+  private List<Long> entregasNaoRealizadas = new ArrayList<>();
+  private Date data;
+
+  private Motorista motorista;
+  private Veiculo veiculo;
+  private List<Encomenda> encomendas = new ArrayList<>();
+
+  public Roteiro() {
+  }
+
+  public Roteiro(Long codigo, Date data, Motorista motorista, Veiculo veiculo) {
+    this.codigo = codigo;
+    this.data = data;
+    this.motorista = motorista;
+    this.veiculo = veiculo;
+  }
+
+  public Long getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(Long codigo) {
+    this.codigo = codigo;
+  }
+
+  public List<Long> getEntregasRealizadas() {
+    return entregasRealizadas;
+  }
+
+  public void setEntregasRealizadas(List<Long> entregasRealizadas) {
+    this.entregasRealizadas = entregasRealizadas;
+  }
+
+  public List<Long> getEntregasNaoRealizadas() {
+    return entregasNaoRealizadas;
+  }
+
+  public void setEntregasNaoRealizadas(List<Long> entregasNaoRealizadas) {
+    this.entregasNaoRealizadas = entregasNaoRealizadas;
+  }
+
+  public Date getData() {
+    return data;
+  }
+
+  public void setData(Date data) {
+    this.data = data;
+  }
+  
+  public Motorista getMotorista() {
+    return motorista;
+  }
+
+  public void setMotorista(Motorista motorista) {
+    this.motorista = motorista;
+  }
+
+  public Veiculo getVeiculo() {
+    return veiculo;
+  }
+
+  public void setVeiculo(Veiculo veiculo) {
+    this.veiculo = veiculo;
+  }
+
+  public List<Encomenda> getEncomendas() {
+    return encomendas;
+  }
+
+  public void setEncomendas(List<Encomenda> encomendas) {
+    this.encomendas = encomendas;
+  }
+
+  public void resgistrarEntregaRealizada(Long codigoEntrega) {
+
+  }
+
+  public void resgistrarEntregaNaoRealizada(Long codigo) {
+
+  }
 }
