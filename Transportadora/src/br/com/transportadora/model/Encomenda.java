e36bfe7ecc2d98034e0ea08dc561cc00db3b0b1b
ACER <ACER@192.168.25.5>
2018-09-23 15:54:04 -0300

    Implementada a classe Encomenda e derivadas

--- Transportadora/src/br/com/transportadora/model/Encomenda.java
+++ Transportadora/src/br/com/transportadora/model/Encomenda.java
@@ -1,6 +1,71 @@
 package br.com.transportadora.model;
 
+import java.util.Date;
 
 public class Encomenda {
-    
+
+  private long proxCodigo;
+  private Remetente remetente;
+  private Destinatario destinatario;
+  private Date datacadastro;
+  private double peso;
+  private Long codigo;
+  private StatusEncomenda statusEncomenda;
+  
+  public long getProxCodigo() {
+    return proxCodigo;
+  }
+
+  public void setProxCodigo(long proxCodigo) {
+    this.proxCodigo = proxCodigo;
+  }
+
+  public Remetente getRemetente() {
+    return remetente;
+  }
+
+  public void setRemetente(Remetente remetente) {
+    this.remetente = remetente;
+  }
+
+  public Destinatario getDestinatario() {
+    return destinatario;
+  }
+
+  public void setDestinatario(Destinatario destinatario) {
+    this.destinatario = destinatario;
+  }
+
+  public Date getDatacadastro() {
+    return datacadastro;
+  }
+
+  public void setDatacadastro(Date datacadastro) {
+    this.datacadastro = datacadastro;
+  }
+
+  public double getPeso() {
+    return peso;
+  }
+
+  public void setPeso(double peso) {
+    this.peso = peso;
+  }
+
+  public Long getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(Long codigo) {
+    this.codigo = codigo;
+  }
+
+  public StatusEncomenda getStatusEncomenda() {
+    return statusEncomenda;
+  }
+
+  public void setStatusEncomenda(StatusEncomenda statusEncomenda) {
+    this.statusEncomenda = statusEncomenda;
+  }
+
 }
--- Transportadora/src/br/com/transportadora/model/Remetente.java
+++ Transportadora/src/br/com/transportadora/model/Remetente.java
@@ -1,5 +1,7 @@
 package br.com.transportadora.model;
 
 public class Remetente {
-    
+    public Remetente(String nome, Endereco endereco){
+      
+    }
 }
