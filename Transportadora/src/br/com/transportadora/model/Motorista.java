914bc3b44fe0b6e1066c5831340051fdc93b702d
jnrw <jnrw@protonmail.com>
2018-09-29 18:17:40 -0300

    Atualizada classe Motorista

--- Transportadora/src/br/com/transportadora/model/Motorista.java
+++ Transportadora/src/br/com/transportadora/model/Motorista.java
@@ -1,22 +1,25 @@
 package br.com.transportadora.model;
 
+import java.time.LocalDate;
+
 
 public class Motorista extends Pessoa{
   
   private Roteiro roteiro;
   private Veiculo veiculo;
   private Cnh cnh;
-
-  
+  private LocalDate nascimento;
+ 
   
   public Motorista(){}
 
-  public Motorista(Roteiro roteiro, Veiculo veiculo, String nome, Endereco endereco, Cnh cnh) {
+  public Motorista(Roteiro roteiro, Veiculo veiculo, Cnh cnh, LocalDate nascimento, String nome, Endereco endereco) {
     super(nome, endereco);
     this.roteiro = roteiro;
     this.veiculo = veiculo;
     this.cnh = cnh;
-  }  
+    this.nascimento = nascimento;
+  } 
 
   public Roteiro getRoteiro() {
     return roteiro;
@@ -33,6 +36,7 @@ public class Motorista extends Pessoa{
   public void setVeiculo(Veiculo veiculo) {
     this.veiculo = veiculo;
   }
+
   public Cnh getCnh() {
     return cnh;
   }
@@ -40,4 +44,13 @@ public class Motorista extends Pessoa{
   public void setCnh(Cnh cnh) {
     this.cnh = cnh;
   }
+
+  public LocalDate getNascimento() {
+    return nascimento;
+  }
+
+  public void setNascimento(LocalDate nascimento) {
+    this.nascimento = nascimento;
+  }
+
 }
